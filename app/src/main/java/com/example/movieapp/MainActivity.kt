f6813d85875e2af1f3b69a94700026f76ff5931e
Regina <xzregina14@gmail.com>
2023-08-29 22:52:43 -0700

    Using LazyColum and Showing Movies

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,18 +4,39 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,19 +55,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit){
     MovieAppTheme {
-        //content()
         Scaffold(
-            modifier = Modifier,
-            contentColor = Color.Magenta,
             topBar = {
                 TopAppBar(
                     title = {
                         Text("Movies")
                     },
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Magenta)
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Magenta),
                 )
-            }){
-           content()
+            }){innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                content()
+            }
         }
     }
 }
@@ -54,9 +78,43 @@ fun MyApp(content: @Composable () -> Unit){
 
 
 @Composable
-fun MainContent(){
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Text(text = "Hello")
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Harry Potter",
+    "Life"
+)){
+    Column(
+        modifier = Modifier.padding(12.dp),
+    ) {
+        LazyColumn() {
+            items(movieList){
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String){
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp))
+    {
+        Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start){
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
     }
 }
 @Composable
