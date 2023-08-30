9e4ebbaca254e10d0f121e69a2ba3638c3df833d
Regina <xzregina14@gmail.com>
2023-08-29 20:34:35 -0700

    add new files

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,31 +1,54 @@
 package com.example.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movieapp.ui.theme.MovieAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
+    }
+}
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    MovieAppTheme {
+        //content()
+        Scaffold(topBar = {
+            TopAppBar(backgroundColor = Color.Magenta,
+                elevation = 0.dp) {
+                Text(text = "Movies")
+            }
+        },) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MainContent(){
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello")
     }
 }
 
@@ -40,7 +63,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
