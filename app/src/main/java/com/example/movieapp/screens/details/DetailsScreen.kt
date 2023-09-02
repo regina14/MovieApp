a786e8cc70de9c883ae8a5f17482518522c625e6
Regina <xzregina14@gmail.com>
2023-09-01 21:29:03 -0700

    Adding the TopAppBar and Back Navigation

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,40 +1,66 @@
 package com.example.movieapp.screens.details
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.movieapp.screens.home.MainContent
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
-    Surface(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text("")
+        }, colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Transparent), navigationIcon = {
+            Row(horizontalArrangement = Arrangement.Start) {
+                Icon(imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Arrow Back",
+                    modifier = Modifier.clickable {
+                        navController.popBackStack()
+                    })
+                Spacer(modifier = Modifier.width(100.dp))
+                Text(text = "Movies")
+            }
+        })
+    }) { innerPadding ->
+        //Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(paddingValues = innerPadding)
         ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go Back")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
             }
         }
     }
-
 }
